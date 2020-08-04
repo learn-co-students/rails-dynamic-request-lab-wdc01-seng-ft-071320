3a37fe1294b27c345c0af1e75838537def735342
Kristian Portillo <kristianportillo@Kristians-MacBook-Air.local>
2020-08-04 17:22:06 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,8 @@
 class StudentsController < ApplicationController
-  def index
-    @students = Student.all
-  end
+    def index
+        @students = Student.all
+    end
+    def show
+        @student = Student.find(params[:id])
+    end
 end
