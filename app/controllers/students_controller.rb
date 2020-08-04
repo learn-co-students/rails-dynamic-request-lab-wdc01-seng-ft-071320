9c7608212b6e7ce86488423ac1a9db0bb406e4b6
chsohn15 <chsohn15@gmail.com>
2020-08-04 16:28:37 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,11 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    id = params[:id]
+    @student = Student.find(id)
+    #render :show
+  end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: "Andrew", last_name: "Park")
+Student.create(first_name: "Christina", last_name: "Sohn")
